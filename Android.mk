b42ac43e43db0ff77b42d167362bdfa2ef6c0707
errantzebra845 <errantzebra845@gmail.com>
2014-05-19 12:08:01 -0400

    Make it CandyWrappers -all credit to Paranoid Android for their wallpaper source

--- Android.mk
+++ Android.mk
@@ -7,16 +7,13 @@
 # or at https://www.gnu.org/licenses/gpl-2.0.txt
 #
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES += $(call all-java-files-under, src)
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4
-
-LOCAL_PACKAGE_NAME := ParanoidWallpapers
+LOCAL_PACKAGE_NAME := CandyWrappers
 
 include $(BUILD_PACKAGE)
